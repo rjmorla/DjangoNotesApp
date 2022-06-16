fd74d1e9a21b398aaa839e05006211a48a321cc6
Roy Morla <royjmorla@gmail.com>
2022-06-15 20:37:07 -0700

    Add code to allow data from api to displayed

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const NotesListPage = () => {
-  return (
-    <div>
-        NotesListPage
-    </div>
-  )
+
+    let [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        getNotes()
+    }, [])
+
+    let getNotes = async () => {
+        let response = await fetch('http://127.0.0.1:8000/api/notes/')
+        let data = await response.json()
+        console.log('DATA:', data)
+        setNotes(data)
+    }
+    
+
+    return (
+        <div>
+            <div className='notes-list'>
+                {notes.map((note, index) => (
+                    <h3 key={index}>{note.body}</h3>
+                ))}
+            </div>
+        </div>
+    )
 }
 
 export default NotesListPage
